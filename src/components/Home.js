5327448b4408a04aa632d18bc364a17eedcfa41d
Shailesh <shlshkrk@gmail.com>
2020-12-04 09:31:35 +1100

    updated infopara to pass font size

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <div className="wrapper">
             <Logo />
-            <InformationPara title="Hi, we are Glen, Peter and Shailesh." body="And, this is a website to showcase some of the projects we have done together." />
+            <InformationPara title="Hi, we are Glen, Peter and Shailesh." body="And, this is a website to showcase some of the projects we have done together." titleFontSize="2rem" bodyFontSize="1.5rem" />
             <LinkButton target="/TechnicalStack" link="Know about our Technology Stack" />
         </div>
     )
