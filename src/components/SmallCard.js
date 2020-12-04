a4a30d230f231750d7c385928be5a5a300f3fdd4
Glen <mistergjones@yahoo.com.au>
2020-12-04 12:56:44 +1100

    Added smallCard, LargeCard with css and updates to home.js home.css

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,11 +1,19 @@
 import React from "react";
-import theImage from "../../public/logo512.png";
 import "../styles/SmallCard.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SmallCard(props) {
     return (
         <div className="card-wrapper">
-            <img src={theImage} alt="" className="card-image" />
+            <div className="font-awesome">
+                <FontAwesomeIcon
+                    icon={props.iconName}
+                    size={"5x"}
+                    color={props.color}
+                />
+            </div>
+
             <h3 className="card-title">{props.title}</h3>
             <p className="card-paragraph">{props.content}</p>
         </div>
