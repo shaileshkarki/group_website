4d3c31d0343ba5218ccfb7e6c34b07e1de4b8b71
Glen <mistergjones@yahoo.com.au>
2020-12-08 15:08:55 +1100

    now includes technology stack components

---
+++ src/components/TechnicalCardStack.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/TechnicalCardStack.css";
+
+export default function TechnicalCardStack(props) {
+    console.log(props);
+    return (
+        <div className="card-wrapper">
+            <img
+                className="card-image"
+                src={props.src}
+                height="30%"
+                width="30%"
+                alt=""
+            />
+            <h3 className="card-title">{props.title}</h3>
+            <p className="card-content">{props.content}</p>
+        </div>
+    );
+}
