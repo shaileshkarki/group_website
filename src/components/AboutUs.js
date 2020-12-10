7675300c0ac159ad884dbffc277c7321c1fa57ad
Peter Hristakos <peter.hristakos@bigpond.com>
2020-12-10 17:15:28 +1100

    Fixed alignment issue with tech stack page, changed layout of about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,11 +4,14 @@ import "../styles/AboutUs.css";
 
 export default function AboutUs(props) {
     return (
-        <div className="card-aboutus-wrapper">
-            <img className="card-aboutus-image" src={theImage} alt="" />
-            <div className="div-wrapper">
+        <div className="about-us-container">
+            <div className="card-aboutus-wrapper">
+                <img className="card-aboutus-image" src={theImage} alt="" />
                 <h3 className="card-aboutus-personName">{props.personName}</h3>
                 <h3 className="card-aboutus-jobTitle">{props.jobTitle}</h3>
+
+            </div>
+            <div className="div-wrapper">
                 <p className="card-aboutus-content">{props.content}</p>
             </div>
         </div>
