1dae93814bd57f9379c168138c02de6957882b91
Peter Hristakos <peter.hristakos@bigpond.com>
2020-12-16 11:32:30 +1100

    added colour theme

--- src/components/FormButton.js
+++ src/components/FormButton.js
@@ -3,7 +3,7 @@ import "../styles/FormButton.css"
 export default function FormButton({ title, width = 250, height, onSubmit, type, margin = 0 }) {
     return (
         <div className="button-wrapper" >
-            <button type={type} onClick={onSubmit} style={{ color: "white", fontSize: "26px", width: "100%", height: height, backgroundColor: "red", borderRadius: "20px", outline: "none" }} >{title}</button>
+            <button type={type} onClick={onSubmit} style={{ color: "white", fontSize: "26px", width: "100%", height: height, backgroundColor: "#c53211", borderRadius: "20px", outline: "none" }} >{title}</button>
         </div>
     )
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import "../styles/TextArea.css";
 
-export default function TextArea({ width = 199, height, error, errorTextColor = "red", margin = 10, placeholder, fontSize = 15, onChange, color = "black" }) {
+export default function TextArea({ width = 199, height, error, errorTextColor = "#c53211", margin = 10, placeholder, fontSize = 15, onChange, color = "black" }) {
     return (
         <div style={{ width: "100%" }}>
             <textarea className="text-area" onChange={onChange} placeholder={placeholder} type="text" style={{ height: height, fontFamily: "sans- serif", fontSize: fontSize, color: color }} />
